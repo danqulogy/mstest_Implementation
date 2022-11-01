805f246f02af9eb538961d65681b2f704067c654
Ben <danquahwhite@gmail.com>
2022-11-01 10:10:01 +0000

    implemented string asserts methods

---
+++ MyClassesTest/StringAssertClassTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace MyClassesTest;
+
+[TestClass]
+public class StringAssertClassTest
+{
+    
+    [TestMethod]
+    public void IsAllLowerCaseTest()
+    {
+        Regex regex = new Regex(@"^([^A-Z])+$");
+        StringAssert.Matches("all are lower case", regex);
+    }
+
+    [TestMethod]
+    public void StartsWithTest()
+    {
+        var name = "Bernard Danquah White";
+        StringAssert.StartsWith(name, "Be");
+    }
+    
+    [TestMethod]
+    public void ContainsTest()
+    {
+        var name = "Bernard Danquah White";
+        StringAssert.Contains(name, "white", StringComparison.CurrentCultureIgnoreCase);
+    }
+}
